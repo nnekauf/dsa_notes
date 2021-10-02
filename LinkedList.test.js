97ad955173a12e3c549b2c06281feadad700ca09
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-10-02 17:28:38 -0400

    adds tests for adding node anywhere in the linked list

--- LinkedList.test.js
+++ LinkedList.test.js
@@ -49,3 +49,46 @@ describe("#getByIndex", () => {
         })
     })
 })
+
+describe("#insertAtIndex", () => {
+    describe("with index less than 0", ()=> {
+        test("it does not insert anything", () => {
+            const ll = LinkedList.fromValues(10, 20)
+            ll.insertAtIndex(-1, 30)
+
+            expect(ll.length).toBe(2)
+        })
+    })
+
+    describe("with index greater than list length", ()=> {
+        test("it does not insert anything", () => {
+            const ll = LinkedList.fromValues(10, 20)
+            ll.insertAtIndex(5, 30)
+
+            expect(ll.length).toBe(2)
+        })
+    })
+
+    describe("with index 0", ()=> {
+        test("should insert at head", () => {
+            const ll = LinkedList.fromValues(10, 20)
+            ll.insertAtIndex(0, 30)
+
+            expect(ll.length).toBe(3)
+            expect(ll.head.value).toBe(30)
+            expect(ll.head.next.value).toBe(10)
+        })
+    })
+
+    describe("with index in the middle", ()=> {
+        test("should insert at the given index", () => {
+            const ll = LinkedList.fromValues(10, 20, 40)
+            ll.insertAtIndex(2, 30)
+            const node = ll.getByIndex(2)
+
+            expect(ll.length).toBe(4)
+            expect(ll.node.value).toBe(30)
+            expect(ll.head.next.value).toBe(40)
+        })
+    })
+})
